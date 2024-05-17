618795dd8bef2ddf46ee6024af948e09acf7f959
Piper <77084364+pipemm@users.noreply.github.com>
2024-05-17 05:13:19 +0000

    get amd64

--- Python/Bash/get_download_url.sh
+++ Python/Bash/get_download_url.sh
@@ -4,5 +4,6 @@ url_windows_download_page='https://www.python.org/downloads/windows/'
 
 curl --compressed "${url_windows_download_page}" |
   sed --silent 's!.* href="\([^"]*\)">\([^<]*\)<.*!\1!p' |
-  sed --silent 's!^http[s]\?://\(.*/python-[0-9]\+[.][0-9]\+[.][0-9]\+-embed-[a-z0-9]\+[.]zip\)!https://\1!p'
+  sed --silent 's!^http[s]\?://\(.*/python-[0-9]\+[.][0-9]\+[.][0-9]\+-embed-[a-z0-9]\+[.]zip\)!https://\1!p' |
+  sed --silent '/-embed-amd64[.]zip$/p'
 
