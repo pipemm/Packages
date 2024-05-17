35a7780a23a0a4ff36e3c759b41f3d0c1f833777
Piper <77084364+pipemm@users.noreply.github.com>
2024-05-17 05:02:46 +0000

    test python

--- Python/Bash/get_download_url.sh
+++ Python/Bash/get_download_url.sh
@@ -2,6 +2,5 @@
 
 curl --compressed 'https://www.python.org/downloads/windows/' |
   sed --silent 's!.* href="\([^"]*\)">\([^<]*\)<.*!\1!p' |
-  sed --silent 's!^http[s]\?://\(.*/python-[0-9]\+[.][0-9]\+[.][0-9]\+-embed-amd64[.]zip\)!https://\1!p'
+  sed --silent 's!^http[s]\?://\(.*/python-[0-9]\+[.][0-9]\+[.][0-9]\+-embed-[a-z0-9]\+[.]zip\)!https://\1!p'
 
-  
