bb725782fcf9ea6404c67d18f8591f19271272f8
Piper <77084364+pipemm@users.noreply.github.com>
2024-05-17 05:06:40 +0000

    testing

--- Python/Bash/get_download_url.sh
+++ Python/Bash/get_download_url.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
 
+url_windows_download_page='https://www.python.org/downloads/windows/'
 
-curl --compressed 'https://www.python.org/downloads/windows/' |
+curl --compressed "${url_windows_download_page}" |
   sed --silent 's!.* href="\([^"]*\)">\([^<]*\)<.*!\1!p' |
   sed --silent 's!^http[s]\?://\(.*/python-[0-9]\+[.][0-9]\+[.][0-9]\+-embed-[a-z0-9]\+[.]zip\)!https://\1!p'
 
