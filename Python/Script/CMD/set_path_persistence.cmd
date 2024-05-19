a8696995201b595a3318102c30d8c1516c30f237
Piper <77084364+pipemm@users.noreply.github.com>
2024-05-19 11:54:12 +0000

    use script file instead

---
+++ Python/Script/CMD/set_path_persistence.cmd
@@ -0,0 +1,11 @@
+
+SET "PYTHON_CONFIG=Python\PATH.txt"
+SET /P PATH_PYTHON= < "%PYTHON_CONFIG%"
+IF EXIST "%PATH_PYTHON%\python.exe" (
+  SET "PATH=%PATH_PYTHON%;%PATH%"
+  ECHO PATH=%PATH%
+  TYPE %GITHUB_ENV%
+) ELSE (
+  ECHO python.exe not found
+  EXIT /B 1
+)
