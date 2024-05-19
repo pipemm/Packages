3e2e0f66f549f86f2c64352cba0f1364e7fed27e
Piper <77084364+pipemm@users.noreply.github.com>
2024-05-19 16:21:27 +0000

    update

--- Python/Script/Python/excel_column.py
+++ Python/Script/Python/excel_column.py
@@ -1,16 +1,23 @@
 
-from sys import stdout
 from csv import DictWriter
 
+from sys import stdout
+from os  import write
+
+class Stdout:
+    def write(self, text):
+        text_bytes = text.encode()
+        write(1, text_bytes)
+
 def main():
-    outfile = stdout
+    outfile = Stdout()
 
     fieldnames = ['first_name', 'last_name']
     writer = DictWriter(outfile, fieldnames=fieldnames)
 
     writer.writeheader()
-    writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
-    writer.writerow({'first_name': 'Lovely', 'last_name': 'Spam'})
+    writer.writerow({'first_name': 'Baked',     'last_name': 'Beans'})
+    writer.writerow({'first_name': 'Lovely',    'last_name': 'Spam'})
     writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
 
 if __name__ == '__main__':
