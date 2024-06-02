867cca974c9c983524b539517d558156a727f176
Piper <77084364+pipemm@users.noreply.github.com>
2024-06-02 18:02:22 +0000

    updating

--- Ruby/download.ps1
+++ Ruby/download.ps1
@@ -2,10 +2,24 @@
 
 [System.String]$URLPage = 'https://rubyinstaller.org/downloads/';
 
+## [Microsoft.PowerShell.Commands.BasicHtmlWebResponseObject]
+## [Microsoft.PowerShell.Commands.HtmlWebResponseObject]
 [Microsoft.PowerShell.Commands.BasicHtmlWebResponseObject]$Response = Invoke-WebRequest -Uri "${URLPage}";
 
-[System.String]$Content = ${Response}.Content;
+[System.String]$Content = ${Response}.Content -split "`n";
 
-($Content -split "`n" |
+[System.String]$LinesMatched = ($Content |
   Select-String -Pattern 'download-recommended').ToString();
 
+if( $LinesMatched -match ' ([0-9]+\.[0-9]+\.[0-9]+-[0-9]+)\b' ) {
+  [System.String]$Version = $Matches[1];
+}
+
+if ( -not ${Version} ) {
+    Write-Host "Version is not found."
+    exit 1
+}
+
+
+$Content |
+  Select-String -Pattern "rubyinstaller-${Version}-x64.7z"
