72c33d3e5124814d802d3da9ab1f14437888820b
Piper <77084364+pipemm@users.noreply.github.com>
2024-06-02 08:33:05 +0000

    update

--- 7z/download_mac.sh
+++ 7z/download_mac.sh
@@ -1,11 +1,33 @@
 
 url_domain='https://www.7-zip.org/'
-url_page='https://www.7-zip.org/download.html'
+url_page="${url_domain%/}/download.html"
 
-curl "${url_page}" |
+url_package=$(
+  curl "${url_page}" |
   sed --silent 's!^.*"\(.*7z.*-mac\.tar\.xz\)".*$!\1!p' |
   head --lines=1 |
   while read -r line
   do
     echo "${url_domain%/}/${line}"
   done
+)
+
+if [[ -z "${url_package}" ]]
+then
+  echo 'download link not found'
+  echo 1
+fi
+
+folder_dl='package7z/'
+mkdir --parent "${folder_dl%/}/"
+
+echo "downloading ${url_package}"
+
+echo "${url_package##*/}"
+
+curl --output "${folder_dl%/}/${url_package##*/}" "${url_package}"
+
+folder_7z='bin7z/'
+mkdir --parent "${folder_7z%/}/"
+
+
