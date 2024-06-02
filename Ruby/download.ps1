c99f88a28d8e733cb5e5093319a5b9eff09ac2b4
Piper <77084364+pipemm@users.noreply.github.com>
2024-06-02 15:50:43 +0000

    test

--- Ruby/download.ps1
+++ Ruby/download.ps1
@@ -1,5 +1,9 @@
 
-[Syste.String]$folder_html='HTML/'
-[Syste.String]$file_html="${folder_html}index.html"
+[System.String]$folder_html='HTML\'
+New-Item -Path . -Name "${folder_html}" -ItemType 'directory'
+New-Item -Path . -Name "${folder_html}a\b\c" -ItemType 'directory'
+
+[System.String]$file_html="${folder_html}index.html"
 
 [System.String]$url_page='https://rubyinstaller.org/downloads/'
+
