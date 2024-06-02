f6d2ba8969cf6eb233cc73174b63ad7ee28a62d4
Piper <77084364+pipemm@users.noreply.github.com>
2024-06-02 16:12:35 +0000

    test

--- Ruby/download.ps1
+++ Ruby/download.ps1
@@ -1,15 +1,8 @@
 
-[System.String]$folder_html='HTML\'
-New-Item -Path . -Name "${folder_html}" -ItemType 'directory'
 
-[System.String]$file_html="${folder_html}index.html"
 [System.String]$url_page='https://rubyinstaller.org/downloads/'
-
-Invoke-WebRequest -Uri "${url_page}" -OutFile "${file_html}"
 
 $Response = Invoke-WebRequest -Uri "${url_page}"
 
 $Response.GetType()
-
-$Response 
 
