931e9ad34732e9cac0489c74f2ae8b9f4031e8dd
Piper <77084364+pipemm@users.noreply.github.com>
2024-06-14 10:58:53 +0000

    update

--- Apache_POI/compile.sh
+++ Apache_POI/compile.sh
@@ -1,4 +1,8 @@
 
+folder_class='local_class/'
+mkdir --parent "${folder_class%/}/"
+folder_xlsx='local_xlsx/'
+mkdir --parent "${folder_xlsx%/}/"
 
 CLASSPATH='.'
 for jar in ${PWD%/}/local_jar/*.jar
@@ -6,8 +10,8 @@ do
     CLASSPATH="${CLASSPATH}:${jar}"
 done
 
-javac --class-path "${CLASSPATH}" 'script_java/NewWorkbook.java'
+javac --class-path "${CLASSPATH}" -d "${folder_class%/}/" 'script_java/NewWorkbook.java'
 
-cd script_java
+CLASSPATH="${CLASSPATH}:${folder_class%/}/"
 
-java  --class-path "${CLASSPATH}"  NewWorkbook newname.xlsx
+java  --class-path "${CLASSPATH}"  NewWorkbook "${folder_xlsx%/}/newname.xlsx"
