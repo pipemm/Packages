65ceff4f93c667e3cb71bc10b66f890e2d1f9579
Piper <77084364+pipemm@users.noreply.github.com>
2024-06-14 10:36:38 +0000

    update

--- Apache_POI/compile.sh
+++ Apache_POI/compile.sh
@@ -1,4 +1,13 @@
 
 
-javac --class-path 'local_jar/*.jar' 'script_java/NewWorkbook.java'
+CLASSPATH='.'
+for jar in ${PWD%/}/local_jar/*.jar
+do
+    CLASSPATH="${CLASSPATH}:${jar}"
+done
 
+javac --class-path "${CLASSPATH}" 'script_java/NewWorkbook.java'
+
+cd script_java
+
+java  --class-path "${CLASSPATH}"  NewWorkbook newname.xlsx
