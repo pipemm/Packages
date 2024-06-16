ab72ca4cc05b062e870ea33d82cf1f40bc586f48
Piper <77084364+pipemm@users.noreply.github.com>
2024-06-16 07:36:44 +0000

    modify excel scripts

--- Apache_POI/compile.sh
+++ Apache_POI/compile.sh
@@ -1,8 +1,6 @@
 
 folder_class='local_class/'
 mkdir --parent "${folder_class%/}/"
-folder_xlsx='local_xlsx/'
-mkdir --parent "${folder_xlsx%/}/"
 
 CLASSPATH='.'
 for jar in ${PWD%/}/local_jar/*.jar
@@ -12,6 +10,3 @@ done
 
 javac --class-path "${CLASSPATH}" -d "${folder_class%/}/" script_java/*.java
 
-CLASSPATH="${CLASSPATH}:${folder_class%/}/"
-
-java  --class-path "${CLASSPATH}"  NewWorkbook "${folder_xlsx%/}/newname.xlsx"
---
+++ Apache_POI/test_newexcel.sh
@@ -0,0 +1,16 @@
+
+folder_class='local_class/'
+mkdir --parent "${folder_class%/}/"
+folder_xlsx='local_xlsx/'
+mkdir --parent "${folder_xlsx%/}/"
+
+CLASSPATH='.'
+for jar in ${PWD%/}/local_jar/*.jar
+do
+    CLASSPATH="${CLASSPATH}:${jar}"
+done
+
+CLASSPATH="${CLASSPATH}:${folder_class%/}/"
+
+java  --class-path "${CLASSPATH}"  NewWorkbook "${folder_xlsx%/}/New Microsoft Excel Worksheet.xlsx"
+
