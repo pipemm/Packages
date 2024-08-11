0df030c5b1ecffeb0752981ca1a655e0d45c25af
Piper <77084364+pipemm@users.noreply.github.com>
2024-08-11 16:30:22 +0000

    update postgredb

---
+++ PostgreSQL/JDBC/Bash/Download_URL.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+REPOSITORY_PGJDBC='pgjdbc/pgjdbc'
+## https://docs.github.com/en/rest/releases/releases?apiVersion=2022-11-28#get-the-latest-release
+API_RELEASE_LATEST="https://api.github.com/repos/${REPOSITORY_PGJDBC}/releases/latest"
+
+URL_DOWNLOAD=$(
+  curl --location \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  "${API_RELEASE_LATEST}" |
+  jq '.assets' |
+  jq '[.[]|select(.name|endswith(".jar"))]' |
+  jq '[.[]|select(.name|startswith("postgresql-"))]' |
+  jq --raw-output '.[0] | .browser_download_url'
+)
+
+echo "${URL_DOWNLOAD}"
+
