d4279bb13087e57438182eb039f3d07f5599547a
Piper <77084364+pipemm@users.noreply.github.com>
2024-08-12 16:51:15 +0000

    update

--- FontForge/Script/Bash/Execution.sh
+++ FontForge/Script/Bash/Execution.sh
@@ -15,6 +15,6 @@ ls Font/*/*.* |
   while read -r file
   do
     echo ${file}
-    ${FontForgeApp} --appimage-extract-and-run -lang=py -script "${pytest}" "${file}"
+    ${FontForgeApp} --appimage-extract-and-run -lang=py -script "${pytest}" "${PWD%/}/${file}"
   done
 
