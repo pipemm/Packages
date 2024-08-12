5cc8b357a679e3eae0aa94773646f3b4a87ae52f
Piper <77084364+pipemm@users.noreply.github.com>
2024-08-12 16:48:14 +0000

    testing

--- FontForge/Script/Python/Test.py
+++ FontForge/Script/Python/Test.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env fontforge
 
 from sys import argv
-import fontforge
+from fontforge import open
 
 def main():
-    print(len(argv))
-    if len(argv)>1:
-        print(argv[0])
-        print(argv[1])
+    if len(argv)<=1:
+        return
+    fileFont = argv[1]
+    font = open(fileFont)
+    # https://github.com/fontforge/fontforge/blob/master/fontforge/psfont.h
+    # https://github.com/fontforge/fontforge/blob/master/doc/sphinx/scripting/python/fontforge.rst
+    print("Font Name: ", font.fontname)
+    print("Full Name: ", font.fullname)
+    print("Family Name: ", font.familyname)
+
 
 if __name__ == '__main__':
     main()
