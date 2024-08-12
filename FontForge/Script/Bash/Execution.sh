172be5f67a9d7f07546e7d278ade089a13bf7002
Piper <77084364+pipemm@users.noreply.github.com>
2024-08-12 16:22:21 +0000

    update py

--- FontForge/Script/Bash/Execution.sh
+++ FontForge/Script/Bash/Execution.sh
@@ -4,14 +4,16 @@ FontForgeApp=$(
   ls App/FontForge-*.AppImage |
   head --lines=1
 )
-
 chmod u+x ${FontForgeApp}
 
 ${FontForgeApp} --appimage-extract-and-run --version 
 
+folderpy='Python/'
+pytest="${folderpy%/}/Test.py"
+
 ls Font/*/*.* |
   while read -r file
   do
-    echo "${file}"
+    ${FontForgeApp} --appimage-extract-and-run -lang=py -script "${file}"
   done
 
