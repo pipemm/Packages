ff52152f35da1ed3eeb79590b8e2ef3326dce943
Piper <77084364+pipemm@users.noreply.github.com>
2024-08-31 18:12:37 +0000

    testing

--- LilyPond/Bash/Generate.sh
+++ LilyPond/Bash/Generate.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/bash
 
-which lilypond
+which -a lilypond
+echo $?
 if [[ $? ]]
 then
   exit 1
 fi
 
+FOLDER_OUT='Output/'
+mkdir --parent "${FOLDER_OUT}"
 FOLDER_CODE='Code/'
 if [[ -d "${FOLDER_CODE}" ]]
 then
