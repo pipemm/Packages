38d21e6b963e740b3845f18c15444d90caeff21c
Piper <77084364+pipemm@users.noreply.github.com>
2024-08-31 15:33:25 +0000

    add new script

--- LilyPond/Bash/Download-Linux.sh
+++ LilyPond/Bash/Download-Linux.sh
@@ -8,7 +8,7 @@ fi
 echo "${URL_PACKAGE##*/}"
 
 FILE_PACKAGE="${URL_PACKAGE##*/}"
-NAME_PACKAGE="${FILE_PACKAGE%.tar.gz}"
+NAME_PACKAGE="${FILE_PACKAGE%.zip}"
 
 FOLDER_PACK="Package/"
 FOLDER_DL="${FOLDER_PACK%/}/Download/"
@@ -16,28 +16,3 @@ mkdir --parent "${FOLDER_DL%/}/"
 
 FILE_PACKAGE="${FOLDER_DL%/}/${FILE_PACKAGE}"
 curl --output "${FILE_PACKAGE}" --location "${URL_PACKAGE}"
-
-TAR_FOLDER=$(
-  tar --list --gunzip --file="${FILE_PACKAGE}" |
-  head --lines=1
-)
-
-tar --extract --gunzip --verbose \
-  --file="${FILE_PACKAGE}" \
-  --directory="${FOLDER_PACK}"
-
-FOLDER_ARTIFACT="${FOLDER_PACK%/}/${TAR_FOLDER%/}/"
-if [[ -d "${FOLDER_ARTIFACT}" ]]
-then
-  FOLDER_ARTIFACT="${PWD%/}/${FOLDER_ARTIFACT%/}/"
-else
-  exit 1
-fi
-
-if [[ -n "${GITHUB_ENV}" ]]
-then
-  echo "FOLDER_ARTIFACT=${FOLDER_ARTIFACT}"
-  echo "FOLDER_ARTIFACT=${FOLDER_ARTIFACT}" >> "${GITHUB_ENV}"
-  echo "NAME_PACKAGE=${NAME_PACKAGE}"
-  echo "NAME_PACKAGE=${NAME_PACKAGE}"       >> "${GITHUB_ENV}"
-fi
