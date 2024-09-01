dc975e5df0ce3f87cf28c743a0123ba4bdfc7f83
Piper <77084364+pipemm@users.noreply.github.com>
2024-09-01 14:34:20 +0000

    test

---
+++ LilyPond/CMD/SetUp.cmd
@@ -0,0 +1,11 @@
+
+REM [Directories Only](https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/for#remarks)
+
+FOR /D %%D in (Package\lilypond-*\bin\) DO (
+  SET "subfolder=%%D"
+  GOTO :found
+)
+:found
+
+ECHO %subfolder%
+ECHO %PATH%
