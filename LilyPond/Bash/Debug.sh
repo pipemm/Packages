ce6622298066faf2ab3f5a97461cb8d39c6359ca
Piper <77084364+pipemm@users.noreply.github.com>
2024-09-01 11:51:00 +0000

    debugging

--- LilyPond/Bash/Debug.sh
+++ LilyPond/Bash/Debug.sh
@@ -4,3 +4,12 @@ ls -l --time-style=full-iso /home/runner/work/Packages/Packages/LilyPond/Package
 
 stat /home/runner/work/Packages/Packages/LilyPond/Package/lilypond-*/share/guile/2.2/ice-9/eval.scm
 stat /home/runner/work/Packages/Packages/LilyPond/Package/lilypond-*/lib/guile/2.2/ccache/ice-9/eval.go
+
+ls Package/lilypond-*/lib/*/*/ccache/*/*.go |
+  while read -r gofile
+  do
+    touch gofile
+  done
+
+ls -l --time-style=full-iso /home/runner/work/Packages/Packages/LilyPond/Package/lilypond-*/share/guile/2.2/ice-9/eval.scm
+ls -l --time-style=full-iso /home/runner/work/Packages/Packages/LilyPond/Package/lilypond-*/lib/guile/2.2/ccache/ice-9/eval.go
