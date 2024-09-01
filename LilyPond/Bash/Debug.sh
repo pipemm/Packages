4735caaa6ce82d5a36dfa974cac9d1563bf0b716
Piper <77084364+pipemm@users.noreply.github.com>
2024-09-01 11:15:45 +0000

    debug

---
+++ LilyPond/Bash/Debug.sh
@@ -0,0 +1,4 @@
+
+ls -a /home/runner/work/Packages/Packages/LilyPond/Package/lilypond-*-linux-x86_64/lib/guile/2.2/ccache/ice-9/eval.go
+ls -a /home/runner/work/Packages/Packages/LilyPond/Package/lilypond-*-linux-x86_64/lib/guile/2.2/ccache/ice-9/eval.go
+
