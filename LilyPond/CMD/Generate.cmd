32e98077de9e159719c62d7f9b4a02f14372741e
Piper <77084364+pipemm@users.noreply.github.com>
2024-09-01 15:49:50 +0000

    add file

--- LilyPond/CMD/Generate.cmd
+++ LilyPond/CMD/Generate.cmd
@@ -1,5 +1,6 @@
 
-FOR %%F in (Code\*.ly) do (
+REM [set parameter](https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/for#remarks)
+FOR %%F IN (Code\*.ly) DO (
   SET "CODEFILE=%%F"
   ECHO "%CODEFILE%"
 )
