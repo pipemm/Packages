147084b4cf7c59c47cd2855b14b4bcecb40f3c6b
Piper <77084364+pipemm@users.noreply.github.com>
2024-09-01 11:22:38 +0000

    debug

--- LilyPond/Bash/Debug.sh
+++ LilyPond/Bash/Debug.sh
@@ -1,4 +1,4 @@
 
-ls -l /home/runner/work/Packages/Packages/LilyPond/Package/lilypond-*/lib/guile/2.2/ccache/ice-9/eval.go
+ls -l /home/runner/work/Packages/Packages/LilyPond/Package/lilypond-*/share/guile/2.2/ice-9/eval.scm
 ls -l /home/runner/work/Packages/Packages/LilyPond/Package/lilypond-*/lib/guile/2.2/ccache/ice-9/eval.go
 
