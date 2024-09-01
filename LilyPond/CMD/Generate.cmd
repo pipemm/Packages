9c3cbde4abed5a91caf7a790a471449b020aba9d
Piper <77084364+pipemm@users.noreply.github.com>
2024-09-01 17:47:28 +0000

    testing

--- LilyPond/CMD/Generate.cmd
+++ LilyPond/CMD/Generate.cmd
@@ -1,6 +1,11 @@
 
+where lilypond
+
+set "FOLDER_OUT=Output\"
+mkdir %FOLDER_OUT%
+set "FOLDER_CODE=Code\"
 REM [set parameter](https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/for#remarks)
-FOR %%F IN (Code\*.ly) DO (
-  SET "CODEFILE=%%F"
+for %%F in (%FOLDER_CODE%*.ly) do (
+  set "CODEFILE=%%F"
   ECHO %CODEFILE%
 )
