7766a496ce4fa228587b10cf017ffff979a17965
Piper <77084364+pipemm@users.noreply.github.com>
2024-09-02 14:28:44 +0000

    update

--- LilyPond/CMD/Generate.cmd
+++ LilyPond/CMD/Generate.cmd
@@ -5,8 +5,13 @@ set "FOLDER_OUT=Output\"
 mkdir %FOLDER_OUT%
 set "FOLDER_CODE=Code\"
 REM [set parameter](https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/for#remarks)
+
+setlocal enabledelayedexpansion
 for %%F in (%FOLDER_CODE%*.ly) do (
   set "CODEFILE=%%F"
   echo !CODEFILE!
+  echo %CODEFILE%
   echo lilypond.exe --output="%FOLDER_OUT%" --pdf --png "!CODEFILE!"
 )
+endlocal
+
