7f6ff7e24892e3f69bf144fb541c2aac17766c92
Piper <77084364+pipemm@users.noreply.github.com>
2024-09-02 14:38:07 +0000

    test

--- LilyPond/CMD/Generate.cmd
+++ LilyPond/CMD/Generate.cmd
@@ -5,13 +5,12 @@ set "FOLDER_OUT=Output\"
 mkdir %FOLDER_OUT%
 set "FOLDER_CODE=Code\"
 REM [set parameter](https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/for#remarks)
-
+REM [enabledelayedexpansion](https://learn.microsoft.com/en-gb/windows-server/administration/windows-commands/setlocal#parameters)
 setlocal enabledelayedexpansion
 for %%F in (%FOLDER_CODE%*.ly) do (
   set "CODEFILE=%%F"
-  echo !CODEFILE!
-  echo %CODEFILE%
-  echo lilypond.exe --output="%FOLDER_OUT%" --pdf --png "!CODEFILE!"
+  echo Compiling !CODEFILE!
+  lilypond.exe --output="%FOLDER_OUT%" --pdf --png "!CODEFILE!"
 )
 endlocal
 
