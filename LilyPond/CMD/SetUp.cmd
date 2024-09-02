aa3bd8952ccc2a7c8c0e1f9e76efff2db3dcb122
Piper <77084364+pipemm@users.noreply.github.com>
2024-09-02 17:11:41 +0000

    update

--- LilyPond/CMD/SetUp.cmd
+++ LilyPond/CMD/SetUp.cmd
@@ -17,7 +17,10 @@ if %ERRORLEVEL% neq 0 (
   exit 1
 )
 
-
+REM The program inspects Scheme source code files (.scm) and their corresponding compiled files (.go). 
+REM If a source code file has a more recent timestamp than its compiled counterpart, a warning message is shown. 
+REM This issue appears to be linked to the GUILE_AUTO_COMPILE=1 setting.
+REM The program might then attempt to recompile the source code, which is likely to result in a fatal error, possibly because the current environment is not configured for compiling.
 REM [assign the current time and date to a file without modifying the file](https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/copy)
 DIR %subfolder%\
 
