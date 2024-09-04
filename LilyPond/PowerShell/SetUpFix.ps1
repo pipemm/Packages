e91cf1ebe6fc978d51fa2a07c81974ad28e5e3ad
Piper <77084364+pipemm@users.noreply.github.com>
2024-09-04 15:28:20 +0000

    update

---
+++ LilyPond/PowerShell/SetUpFix.ps1
@@ -0,0 +1,15 @@
+
+[System.String]$PathInstall = 'Package\';
+[System.String]$FolderLilyPond = (
+    Get-ChildItem -Path "${PathInstall}lilypond-*\" -Directory |
+    ForEach-Object -Process {
+        [System.String]$PathFound = $_.FullName;
+        ${PathFound};
+    } |
+    Select-Object -First 1
+);
+
+if ( ${FolderLilyPond} -eq '' )
+{
+    Exit 1;
+}
