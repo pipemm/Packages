22d2401fa17a2aa3645ba25e77e180907be90d51
Piper <77084364+pipemm@users.noreply.github.com>
2024-09-11 14:24:49 +0000

    add hadoop

---
+++ Hadoop/Bash/TestEnvironment.sh
@@ -0,0 +1,23 @@
+
+
+which -a which
+man which
+
+which -a bash
+bash --version
+bash --help
+
+which -a curl
+curl --version
+curl --help
+
+which -a java
+java --version
+java --help
+
+which -a javac
+javac --version
+javac --help
+
+echo "JAVA_HOME=${JAVA_HOME}"
+echo "PATH=${PATH}"
---
+++ Hadoop/Bash/TestSystem.sh
@@ -0,0 +1,14 @@
+
+
+which -a lsb_release
+lsb_release --version
+lsb_release -- help
+
+which -a uname
+uname --version
+uname --help
+
+lsb_release --all
+uname --all
+
+cat /etc/os-release
