5890448df4ce370e96020badeef4706b9f816734
Piper <77084364+pipemm@users.noreply.github.com>
2024-09-12 15:19:42 +0000

    test

--- Hadoop/Bash/GetURL-HIVE-Common2.sh
+++ Hadoop/Bash/GetURL-HIVE-Common2.sh
@@ -42,3 +42,32 @@ then
 fi
 
 HADOOP_VERSION="${URL_TARBALL##*/}"
+
+URL_SHA512=$(
+  cat "${FILE_URLS}" |
+  grep --fixed-strings "/${HADOOP_VERSION}.sha512" --ignore-case |
+  head --lines=1
+)
+
+URL_ASC=$(
+  cat "${FILE_URLS}" |
+  grep --fixed-strings "/${HADOOP_VERSION}.asc" --ignore-case |
+  head --lines=1
+)
+
+if [[ -z "${GITHUB_OUTPUT}" ]]
+then
+  exit 0
+fi
+
+if [[ -n "${URL_SHA512}" ]]
+then
+  echo "URL_SHA512=${URL_SHA512}"
+  echo "URL_SHA512=${URL_SHA512}" >> "${GITHUB_OUTPUT}"
+fi
+
+if [[ -n "${URL_ASC}" ]]
+then
+  echo "URL_ASC=${URL_ASC}"
+  echo "URL_ASC=${URL_ASC}" >> "${GITHUB_OUTPUT}"
+fi
