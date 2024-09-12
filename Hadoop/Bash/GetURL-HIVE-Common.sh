ef4263030717964530842848abcba6ad924aef7e
Piper <77084364+pipemm@users.noreply.github.com>
2024-09-12 13:53:41 +0000

    get url

--- Hadoop/Bash/GetURL-HIVE-Common.sh
+++ Hadoop/Bash/GetURL-HIVE-Common.sh
@@ -4,5 +4,7 @@
 ## - http://www.apache.org/dyn/closer.cgi/hadoop/common/
 URL_COMMON='https://www.apache.org/dyn/closer.cgi/hadoop/common/'
 curl "${URL_COMMON}" |
-  sed '/Alternate download locations are suggested below\./q'
+  sed '/Alternate download locations are suggested below\./q' |
+  sed '0,/We suggest the following location for your download:/d' |
+  sed --silent 's!^.*"\(http[s]\?[^"]\+\)".*$!\1!p'
 
