02169e09015e33719daabeb4a2c7ca096cddda12
Piper <77084364+pipemm@users.noreply.github.com>
2024-09-12 13:46:11 +0000

    update

--- Hadoop/Bash/GetURL-HIVE-Common.sh
+++ Hadoop/Bash/GetURL-HIVE-Common.sh
@@ -3,5 +3,6 @@
 ## https://hadoop.apache.org/docs/stable/hadoop-project-dist/hadoop-common/SingleCluster.html
 ## - http://www.apache.org/dyn/closer.cgi/hadoop/common/
 URL_COMMON='https://www.apache.org/dyn/closer.cgi/hadoop/common/'
-curl "${URL_COMMON}"
+curl "${URL_COMMON}" |
+  sed '/Alternate download locations are suggested below\./q'
 
