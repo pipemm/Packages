fe4bb996dbdd8511a9a321f9fae8ee39a8797413
Piper <77084364+pipemm@users.noreply.github.com>
2024-11-19 03:14:41 +0000

    update

--- Hadoop/Bash/Install-HADOOP.sh
+++ Hadoop/Bash/Install-HADOOP.sh
@@ -17,16 +17,16 @@ echo 'SHA512Sum Calculation'
 cat "${LOC_HADOOP}" |
   sha512sum
 
-if [[ -n "${CHECK_SHA512}" ]]
+if [[ -n "${URL_SHA512}" ]]
 then
   echo
   echo 'SHA512 Check'
-  echo "${CHECK_SHA512}"
+  curl "${URL_SHA512}"
 fi
 
 FOLDER_INS='Installation/'
 mkdir --parent "${FOLDER_INS%/}/"
 
-tar -xzf "${LOC_HADOOP}" -C "${FOLDER_INS%/}/"
+tar --extract --gunzip --file="${LOC_HADOOP}" --directory="${FOLDER_INS%/}/"
 
 ls ${FOLDER_INS%/}/*.*
