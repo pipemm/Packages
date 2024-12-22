4f9d7606647a89edd7065e9b46c2fb03bbfa8483
Piper <77084364+pipemm@users.noreply.github.com>
2024-12-22 19:41:06 +0000

    retest unknow issue

--- PostgreSQL/Script/Java/demo/GeneralCSVWriter.java
+++ PostgreSQL/Script/Java/demo/GeneralCSVWriter.java
@@ -3,78 +3,39 @@ package demo;
 import java.nio.file.Paths; 
 import java.nio.file.Path; 
 import java.io.File;
+import java.io.IOException;
 
 import java.io.FileWriter;
-import java.io.BufferedWriter;
+//import java.io.BufferedWriter;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.io.IOException;
 
-import com.opencsv.ICSVWriter;
-import com.opencsv.CSVWriterBuilder;
-import com.opencsv.CSVWriter;
-import com.opencsv.ResultSetHelperService;
-
-import org.supercsv.io.ICsvResultSetWriter;
-import org.supercsv.io.CsvResultSetWriter;
-import org.supercsv.prefs.CsvPreference;
-
+import demo.WriterCSVHelper;
 import demo.IWriter;
-//import demo.BufferedWriter; // use the custom BufferedWriter instead of the standard one
+import demo.BufferedWriter; // use the custom BufferedWriter instead of the standard one
 
 public class GeneralCSVWriter implements IWriter {
 
     private File file;
 
     public GeneralCSVWriter(String p) {
-
         String filePath = p;
         Path path       = Paths.get(filePath);
         if( path.getNameCount() == 0 ) {
             throw new IllegalArgumentException("wrong path " + path);
         }
         file = path.toFile();
-
-    }
-
-    private void writeOpenCSV(ResultSet rs) throws SQLException {
-        // https://opencsv.sourceforge.net/
-        boolean includeColumnNames = true;
-        try (
-            BufferedWriter out = new BufferedWriter(new FileWriter(file.toString()));
-        ) {
-            ResultSetHelperService service = new ResultSetHelperService();
-            service.setDateFormat("yyyy-MM-dd");
-            service.setDateTimeFormat("yyyy-MM-dd HH:mm:ss");
-            ICSVWriter csvWriter = 
-                new CSVWriterBuilder(out)
-                    .withResultSetHelper(service)
-                    .build();
-            csvWriter.writeAll(rs, includeColumnNames);
-            out.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException("writer error");
-        }
-    }
-
-    private void writeSuperCSV(ResultSet rs) throws SQLException {
-        // https://super-csv.github.io/super-csv/examples_jdbc.html
-        try (
-            BufferedWriter out         = new BufferedWriter(new FileWriter(file.toString()));
-            ICsvResultSetWriter writer = new CsvResultSetWriter(out, CsvPreference.STANDARD_PREFERENCE);
-        ) {
-            writer.write(rs);
-            out.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException("writer error");
-        }
     }
 
     public void write(ResultSet rs) throws SQLException {
-        writeSuperCSV(rs);
+        try {
+            BufferedWriter out = new BufferedWriter(new FileWriter(file.toString()));
+            WriterCSVHelper.writeSuperCSV(rs,out);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("writer error");
+        }
     }
 
 }
--- PostgreSQL/Script/Java/demo/WriterCSVHelper.java
+++ PostgreSQL/Script/Java/demo/WriterCSVHelper.java
@@ -1,10 +1,9 @@
 package demo;
 
-import java.io.Writer;
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.io.IOException;
+//import java.io.BufferedWriter;
 
 import com.opencsv.ICSVWriter;
 import com.opencsv.CSVWriterBuilder;
@@ -14,36 +13,29 @@ import com.opencsv.ResultSetHelperService;
 import org.supercsv.io.ICsvResultSetWriter;
 import org.supercsv.io.CsvResultSetWriter;
 import org.supercsv.prefs.CsvPreference;
+import demo.BufferedWriter; // use the custom BufferedWriter instead of the standard one
 
 public class WriterCSVHelper {
 
-    private static void writeOpenCSV(ResultSet rs, Writer w) throws SQLException {
+    public static void writeOpenCSV(ResultSet rs, BufferedWriter w) throws SQLException, IOException {
         // https://opencsv.sourceforge.net/
         boolean includeColumnNames = true;
-        try {
-            ResultSetHelperService service = new ResultSetHelperService();
-            service.setDateFormat("yyyy-MM-dd");
-            service.setDateTimeFormat("yyyy-MM-dd HH:mm:ss");
-            ICSVWriter csvWriter = 
-                new CSVWriterBuilder(w)
-                    .withResultSetHelper(service)
-                    .build();
-            csvWriter.writeAll(rs, includeColumnNames);
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException("writer error");
-        }
+        ResultSetHelperService service = new ResultSetHelperService();
+        service.setDateFormat("yyyy-MM-dd");
+        service.setDateTimeFormat("yyyy-MM-dd HH:mm:ss");
+        ICSVWriter csvWriter = 
+            new CSVWriterBuilder(w)
+                .withResultSetHelper(service)
+                .build();
+        csvWriter.writeAll(rs, includeColumnNames);
+        w.flush();
     }
 
-    private static void writeSuperCSV(ResultSet rs, Writer w) throws SQLException {
+    public static void writeSuperCSV(ResultSet rs, BufferedWriter w) throws SQLException, IOException {
         // https://super-csv.github.io/super-csv/examples_jdbc.html
-        try {
-            ICsvResultSetWriter writer = new CsvResultSetWriter(w, CsvPreference.STANDARD_PREFERENCE);
-            writer.write(rs);
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException("writer error");
-        }
+        ICsvResultSetWriter writer = new CsvResultSetWriter(w, CsvPreference.STANDARD_PREFERENCE);
+        writer.write(rs);
+        w.flush();
     }
 
 }
