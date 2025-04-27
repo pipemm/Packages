588921c15235484034d4d7e1be5c692b52c5f2f8
Piper <77084364+pipemm@users.noreply.github.com>
2025-04-27 17:40:59 +0000

    update..

--- Node/Bash/Download-arm64.sh
+++ Node/Bash/Download-arm64.sh
@@ -15,5 +15,12 @@ url_download="${url_prefix%/}/${NODE_VERSION}/node-${NODE_VERSION}-darwin-arm64.
 filename="${url_download##*/}"
 targz_download="${FolderDownload%/}/${filename}"
 curl --output "${targz_download}" "${url_download}"
-unzip "${targz_download}" -d "${FolderUnzipped%/}/"
+tar --extract --gunzip --file="${targz_download}" --directory="${FolderUnzipped%/}/"
 
+ArtifactName="${filename%.tar.gz}"
+
+if [[ -n "${GITHUB_ENV}" ]]
+then
+  echo "ArtifactName=${ArtifactName}"
+  echo "ArtifactName=${ArtifactName}" >> "${GITHUB_ENV}"
+fi
