aafb5541c634c9b20648c7147c9567b5bb59a7fe
Piper <77084364+pipemm@users.noreply.github.com>
2025-04-27 18:06:17 +0000

    add linux

---
+++ Node/Bash/Download-linux-x64.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+if [[ -z "${NODE_VERSION}" ]]
+then
+  exit 1
+fi
+
+FolderReleases='Releases/'
+FolderDownload='Download/'
+mkdir --parent "${FolderDownload%/}/"
+FolderUnzipped='Unzipped/'
+
+url_prefix='https://nodejs.org/dist/'
+url_download="${url_prefix%/}/${NODE_VERSION}/node-${NODE_VERSION}-linux-x64.tar.xz"
+filename="${url_download##*/}"
+targz_download="${FolderDownload%/}/${filename}"
+curl --output "${targz_download}" "${url_download}"
+tar --extract --xz --file="${targz_download}" --directory="${FolderUnzipped%/}/"
+
+ArtifactName="${filename%.tar.gz}"
+
+if [[ -n "${GITHUB_ENV}" ]]
+then
+  echo "ArtifactName=${ArtifactName}"
+  echo "ArtifactName=${ArtifactName}" >> "${GITHUB_ENV}"
+fi
