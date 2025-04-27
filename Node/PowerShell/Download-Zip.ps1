1cbab956e6248310ca0c16b17ba7876f14b13fd5
Piper <77084364+pipemm@users.noreply.github.com>
2025-04-27 16:54:17 +0000

    update

--- Node/PowerShell/Download-Zip.ps1
+++ Node/PowerShell/Download-Zip.ps1
@@ -3,8 +3,13 @@ if ( [System.Environment]::GetEnvironmentVariable('NODE_VERSION') -eq ${null} ) 
   exit 1;
 }
 
+[System.String]${FolderDownload} = 'Download\'
+New-Item -Path "${FolderDownload}" -ItemType Directory -Force;
+[System.String]${FolderDownload} = (Resolve-Path -Path "${FolderDownload}");
+
+
 [System.String]${NodeVersion} = [System.Environment]::GetEnvironmentVariable('NODE_VERSION');
-
-[System.String]${Uri} = 'https://nodejs.org/dist/${NodeVersion}/node-${NodeVersion}-win-x64.zip';
-
+[System.String]${Uri}         = "https://nodejs.org/dist/${NodeVersion}/node-${NodeVersion}-win-x64.zip";
+[System.String]${FileName}    = [System.IO.Path]::GetFileName("${Uri}");
 ${Uri};
+Join-Path -Path "${FolderDownload}" -ChildPath "${FileName}";
