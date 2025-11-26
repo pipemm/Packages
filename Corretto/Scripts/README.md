3dc0e52f69a17915b0093e565a014c45a408bdf5
Piper <77084364+pipemm@users.noreply.github.com>
2025-11-26 04:19:17 +0000

    update

---
+++ Corretto/Scripts/README.md
@@ -0,0 +1,14 @@
+
+
+## Corretto's Support Calendar
+
+[FAQs](https://aws.amazon.com/corretto/faqs/ "Frequently Asked Questions")
+
+| LTS Version | Last Planned Update |
+|-------------|---------------------|
+| Corretto 8  |     October 2030    |
+| Corretto 11 |     October 2031    |
+| Corretto 17 |        July 2029    |
+| Corretto 21 |        July 2030    |
+| Corretto 25 |        July 2032    |
+
---
+++ Corretto/Scripts/get-latest-url.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+## https://docs.aws.amazon.com/corretto/latest/corretto-17-ug/downloads-list.html
+## https://aws.amazon.com/corretto/faqs/#support_calendar
+
+corretto_version=17
+cpu_arch='x64'
+host_os='linux'     ## 'linux' 'al2023'
+package_type='jdk'  ## 'jdk' 'jre'
+file_extension='tar.gz'
+
+url_domain='https://corretto.aws/'
+url_latest_original="${url_domain%/}/downloads/latest/amazon-corretto-${corretto_version}-${cpu_arch}-${host_os}-${package_type}.${file_extension}"
+
+curl --silent --head "${url_latest_original}" |
+  sed --silent 's!^location: !!p' |
+  head --lines=1 |
+  while read -r url_file
+  do
+    echo "${url_domain%/}/${url_file#/}"
+  done
+
