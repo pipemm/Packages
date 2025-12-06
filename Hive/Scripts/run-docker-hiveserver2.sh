2700d0ae2346753da3cb3b31d7d2e12d3fcd1f00
Piper <77084364+pipemm@users.noreply.github.com>
2025-12-06 07:45:27 +0000

    update

--- Hive/Scripts/run-docker-hiveserver2.sh
+++ Hive/Scripts/run-docker-hiveserver2.sh
@@ -6,14 +6,15 @@
 ## - https://github.com/apache/hive/blob/master/packaging/src/docker/build.sh
 
 HIVE_VERSION='3.1.3'
-IMAGE_NAME="apache/hive:${HIVE_VERSION}"
+IMAGE_NAME_TAG="apache/hive:${HIVE_VERSION}"
 CONTAINER_NAME='hive3'
 
 docker run --detach \
   --publish 10000:10000 --publish 10002:10002 \
   --env SERVICE_NAME=hiveserver2 \
-  --name hive3 apache/hive:${HIVE_VERSION}
+  --name "${CONTAINER_NAME}" "${IMAGE_NAME_TAG}"
 
 docker ps
 
 docker logs "${CONTAINER_NAME}"
+
