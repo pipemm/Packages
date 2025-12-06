d3ad050f1092079679d3809b9591ae0eeace953b
Piper <77084364+pipemm@users.noreply.github.com>
2025-12-06 07:43:00 +0000

    update

--- Hive/test-docker.sh
+++ Hive/test-docker.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/bash
 
 ## https://hive.apache.org/development/quickstart/
-## https://hub.docker.com/r/apache/hive/tags
-## https://hub.docker.com/r/apache/hive/tags
+## - https://hub.docker.com/r/apache/hive/tags
+## https://github.com/apache/hive/blob/master/packaging/src/docker/entrypoint.sh
+## - https://github.com/apache/hive/blob/master/packaging/src/docker/build.sh
 
 HIVE_VERSION='3.1.3'
+IMAGE_NAME="apache/hive:${HIVE_VERSION}"
+CONTAINER_NAME='hive3'
 
-##docker pull "${HIVE_VERSION}"
+docker run --detach \
+  --publish 10000:10000 --publish 10002:10002 \
+  --env SERVICE_NAME=hiveserver2 \
+  --name hive3 apache/hive:${HIVE_VERSION}
 
-docker run -d -p 10000:10000 -p 10002:10002 --env SERVICE_NAME=hiveserver2 --name hive4 apache/hive:${HIVE_VERSION}
+docker ps
+
+docker logs "${CONTAINER_NAME}"
