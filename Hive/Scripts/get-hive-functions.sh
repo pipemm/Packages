dfeb5984e40bf80e81506ceaa74a4d398367c1d2
Piper <77084364+pipemm@users.noreply.github.com>
2025-12-07 13:05:46 +0000

    testing again

--- Hive/Scripts/get-hive-functions.sh
+++ Hive/Scripts/get-hive-functions.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 
 hive-query() {
-  local sql_statement="$1"
+  local sql_query="$1"
   env HADOOP_CLIENT_OPTS='-Ddisable.quoting.for.sv=false' beeline \
     -u 'jdbc:hive2://localhost:10000/' \
     --outputformat=csv2 --showHeader=true --nullemptystring=true \
     --hivevar hive.resultset.use.unique.column.names=false \
     --silent=false \
-    -e "${sql_statement}"
+    -e "${sql_query}"
 }
 
 hive-query 'SHOW FUNCTIONS;'
