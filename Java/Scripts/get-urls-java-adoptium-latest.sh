2b901dc218a563181e062846a99779dbd25d39a0
Piper <77084364+pipemm@users.noreply.github.com>
2025-12-09 15:18:15 +0000

    update

--- Java/Scripts/get-urls-java-adoptium-latest.sh
+++ Java/Scripts/get-urls-java-adoptium-latest.sh
@@ -3,12 +3,19 @@
 ## https://adoptium.net/en-GB/temurin/releases
 ## https://api.adoptium.net/q/swagger-ui/
 url_api_server='https://api.adoptium.net/'
-url_api_versions='https://api.adoptium.net/v3/info/available_releases'
+url_api_releases="${url_api_server%/}/v3/info/available_releases"
+jvm_impl='hotspot'
 
-curl "${url_api_versions}" |
+curl "${url_api_releases}" |
   jq --raw-output '.available_releases | sort_by(.) | reverse | .[]' |
   head --lines=1 |
   while read -r feature_version
   do
-    echo "${feature_version}"
+    url_api_assets="${url_api_server%/}/v3/assets/latest/${feature_version}/${jvm_impl}"
+    curl "${url_api_assets}" |
+      jq '[.[].binary]' |
+      jq '[.[] | select(.os | IN("linux", "windows", "mac"))]' |
+      jq '[.[] | select(.image_type | IN("jdk", "jre"))]' |
+      jq '[.[] | select(.architecture == "x64")]' |
+      jq '[.[] | {architecture,image_type,os,package}]'
   done
