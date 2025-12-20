d455671f1eebf76b8c2439e68d46db23f8431846
Piper <77084364+pipemm@users.noreply.github.com>
2025-12-20 04:21:11 +0000

    java test

--- Corretto/Scripts/get-corretto-17-checksum.sh
+++ Corretto/Scripts/get-corretto-17-checksum.sh
@@ -8,6 +8,8 @@ host_os='linux'     ## 'linux'  'al2023'
 package_type='jdk'  ## 'jdk'    'jre'
 file_extension='tar.gz'
 
+## md5    : checksum
+## sha256 : sha256
 checksum="${1,,}"
 if [[ "${checksum}" != 'sha256' ]]
 then
@@ -17,4 +19,4 @@ fi
 url_domain='https://corretto.aws/'
 url_latest_checksum="${url_domain%/}/downloads/latest_${checksum}/amazon-corretto-${corretto_version}-${cpu_arch}-${host_os}-${package_type}.${file_extension}"
 
-curl --silent "${url_latest_checksum}"
+curl --silent --show-error "${url_latest_checksum}"
