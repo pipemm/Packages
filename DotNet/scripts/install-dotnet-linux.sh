1a66b1df2754c30773cc5ec60be8aa161e0cd9e2
Piper <77084364+pipemm@users.noreply.github.com>
2026-01-27 09:54:16 +0000

    install shell

--- DotNet/scripts/install-dotnet-linux.sh
+++ DotNet/scripts/install-dotnet-linux.sh
@@ -10,4 +10,18 @@ FolderPack="${FolderHome%/}/.packages/"
 FolderDown="${FolderPack%/}/Downloads/DotNet/"
 FolderInstall="${FolderPack%/}/${InstallName}/"
 
-find "${FolderDown%/}/" -type f -name '*.tar.gz' -print
+find "${FolderDown%/}/" -type f -name '*.tar.gz' -print |
+  sort --reverse --version-sort |
+  head --lines=1 |
+  while read -r pack
+  do
+    packname="${pack##*/}"
+    packname="${packname%.tar.gz}"
+    FolderInstall2="${FolderInstall%/}/${packname}/"
+    echo "${FolderInstall2}"
+    if [[ ! -f "${FolderInstall2%/}/" ]]
+    then
+      tar --list --ungzip --file="${pack}" |
+        head --lines=5
+    fi
+  done
