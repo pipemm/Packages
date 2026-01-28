85381f9222b815b53815ca8716d71d6405a3f0b4
Piper <77084364+pipemm@users.noreply.github.com>
2026-01-28 05:21:03 +0000

    update

--- DotNet/install_runtime.sh
+++ DotNet/install_runtime.sh
@@ -6,11 +6,7 @@ cd "${ThisPath%/}/"
 
 sh_down='scripts/download-dotnet-linux.sh'
 sh_install='scripts/install-dotnet-linux.sh'
-sh_env='scripts/env-dotnet-linux.sh'
-sh_test='scripts/test-env-linux.sh'
 
 bash "${sh_down}" 'sdk-lts'
 bash "${sh_install}"
 
-source "${sh_env}"
-bash "${sh_test}"
